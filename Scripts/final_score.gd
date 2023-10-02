6346199bd650ee90455a26f17c5fe77a25b7ab16
Noah <noahsimpson15@gmail.com>
2023-10-02 02:38:14 -0700

    updated music and levels

---
+++ Scripts/final_score.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var final_text:Label = $ScoreLabel
+var menu = load("res://Scenes/Main_menu.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	final_text.text = "Final Score " + str(Globals.real_score)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Attack"):
+		LevelChanger.change_level("Fade", menu)
